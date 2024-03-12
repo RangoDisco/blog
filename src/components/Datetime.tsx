4b920483ccd83f6178c2a6d31ef08591964e356e
Maxime <diasmaxime1@gmail.com>
2024-03-12 18:51:11 +0100

    feat(post): handled reading time display

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -8,39 +8,44 @@ interface DatetimesProps {
 interface Props extends DatetimesProps {
   size?: "sm" | "lg";
   className?: string;
+  readingTime: string;
 }
 
 export default function Datetime({
   pubDatetime,
   modDatetime,
   size = "sm",
+  readingTime,
   className,
 }: Props) {
   return (
-    <div className={`flex items-center space-x-2 opacity-80 ${className}`}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className={`${
-          size === "sm" ? "scale-90" : "scale-100"
-        } inline-block h-6 w-6 min-w-[1.375rem] fill-skin-base`}
-        aria-hidden="true"
-      >
-        <path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z"></path>
-        <path d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z"></path>
-      </svg>
-      {modDatetime && modDatetime > pubDatetime ? (
+    <div className={`flex flex-col ${className}`}>
+      <div className={`flex items-center space-x-2 opacity-80 ${className}`}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className={`${
+            size === "sm" ? "scale-90" : "scale-100"
+          } inline-block h-6 w-6 min-w-[1.375rem] fill-skin-base`}
+          aria-hidden="true"
+        >
+          <path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z"></path>
+          <path d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z"></path>
+        </svg>
+        {modDatetime && modDatetime > pubDatetime ? (
+          <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
+            Updated:
+          </span>
+        ) : (
+          <span className="sr-only">Published:</span>
+        )}
         <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
-          Updated:
+          <FormattedDatetime
+            pubDatetime={pubDatetime}
+            modDatetime={modDatetime}
+          />
         </span>
-      ) : (
-        <span className="sr-only">Published:</span>
-      )}
-      <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
-        <FormattedDatetime
-          pubDatetime={pubDatetime}
-          modDatetime={modDatetime}
-        />
-      </span>
+      </div>
+      <span className="px-2"> {readingTime}</span>
     </div>
   );
 }
@@ -50,13 +55,13 @@ const FormattedDatetime = ({ pubDatetime, modDatetime }: DatetimesProps) => {
     modDatetime && modDatetime > pubDatetime ? modDatetime : pubDatetime
   );
 
-  const date = myDatetime.toLocaleDateString(LOCALE.langTag, {
+  const date = myDatetime.toLocaleDateString("en-GB", {
     year: "numeric",
-    month: "short",
+    month: "long",
     day: "numeric",
   });
 
-  const time = myDatetime.toLocaleTimeString(LOCALE.langTag, {
+  const time = myDatetime.toLocaleTimeString("en-GB", {
     hour: "2-digit",
     minute: "2-digit",
   });
@@ -64,7 +69,7 @@ const FormattedDatetime = ({ pubDatetime, modDatetime }: DatetimesProps) => {
   return (
     <>
       <time dateTime={myDatetime.toISOString()}>{date}</time>
-      <span aria-hidden="true"> | </span>
+      <span aria-hidden="true"> </span>
       <span className="sr-only">&nbsp;at&nbsp;</span>
       <span className="text-nowrap">{time}</span>
     </>
